4b7ec8a7cf6b74aeda4da294ac769cb62bab968b
guzmansantanacola <guzmansantanacola@gmail.com>
2026-09-15 00:37:07 -0300

    Configurar deploy en GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/entrega-guzman-santana">
       <CartProvider>
         <App />
       </CartProvider>
